8694607772652b6c6d2a64741cd77828169a1bf1
Md Rehan Ali <rehanali21d@gmail.com>
2024-07-04 20:51:46 +0600

    Added private route

--- house-hunter-client/src/Routes/PrivateRoutes.jsx
+++ house-hunter-client/src/Routes/PrivateRoutes.jsx
@@ -1,6 +1,13 @@
+import { useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 
 const PrivateRoutes = () => {
+    const {user, loading} = useAuth()
+    const location = useLocation()
+
+    if (loading) 
+        return 
     return (
         <div>
             
